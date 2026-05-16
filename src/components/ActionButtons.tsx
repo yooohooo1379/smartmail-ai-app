b3f1c6e58aaadd2553093dd25fc591d54ab888c8
AmirHossein Roushenas <63043725+yooohooo1379@users.noreply.github.com>
2026-05-17 00:50:39 +0330

    Create ActionButtons.tsx
    add action buttons component

---
+++ src/components/ActionButtons.tsx
@@ -0,0 +1,15 @@
+export default function ActionButtons() {
+  return (
+    <div
+      style={{
+        display: "flex",
+        gap: "10px",
+        marginTop: "20px",
+      }}
+    >
+      <button>Summarize Email</button>
+
+      <button>Generate Reply</button>
+    </div>
+  );
+}
